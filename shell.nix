1d6ff7c8a2e405be6efbda81d0cca3b3f3d75140
Sebastian Wålinder <s.walinder@gmail.com>
2019-09-14 23:54:00 +0200

    Now builds with reflex 0.5, added reflex example as main

--- shell.nix
+++ shell.nix
@@ -1,2 +1,21 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc864" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+let
+  inherit (lib) makeLibraryPath;
+  hs = haskell.packages.ghc864;
+  tools = [
+  ];
+  libraries = [
+    SDL2
+    SDL2_image
+    pkg-config
+  ];
+  libraryPath = "${makeLibraryPath libraries}";
+in
+  pkgs.runCommand "shell" {
+    buildInputs = tools ++ libraries;
+    shellHook = ''
+      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${libraryPath}"
+      export LIBRARY_PATH="${libraryPath}"
+    '';
+  } ""
