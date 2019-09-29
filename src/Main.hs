3e433abfda1922c33fd516924aa1b98c640c2998
Sebastian Wålinder <s.walinder@gmail.com>
2019-09-29 22:18:09 +0200

    Installed dunai, bearriver. Setup basic example

--- src/Main.hs
+++ src/Main.hs
@@ -1,82 +1,18 @@
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE LambdaCase            #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE UndecidableInstances  #-}
-module Main where
+import FRP.BearRiver as B
+import FRP.Yampa as Y
+import Control.Concurrent
+  
+type Pos = Double
+type Vel = Double
 
-import           Control.Concurrent   (threadDelay)
-import           Control.Monad        (forM_, guard, void)
-import           Control.Monad.Reader (MonadReader (..), runReaderT)
-import           Reflex
-import           Reflex.SDL2
-import qualified GHC.Word
-
-import qualified MyHost
-
---------------------------------------------------------------------------------
--- | Renders an AABB using the handy SDL 2d 'Renderer'.
-renderAABB :: MonadIO m => Renderer -> V4 Int -> V2 Int -> m ()
-renderAABB r color pos = do
-  rendererDrawColor r $= (fromIntegral <$> color)
-  fillRect r $ Just $ Rectangle (P $ fromIntegral <$> pos - 10) 20
-
--------------------------------------------------------------------------------
--- | A type representing one layer in our app.
-type Layer m = Performable m ()
-
-----------------------------------------------------------------------
--- | Commit one layer that changes over time.
-commitLayer :: (ReflexSDL2 t m, MonadDynamicWriter t [Layer m] m)
-            => Dynamic t (Layer m) -> m ()
-commitLayer = tellDyn . fmap pure
-
-guest
-  :: (ReflexSDL2 t m, MonadDynamicWriter t [Layer m] m, MonadReader Renderer m)
-  => m ()
-guest = do
-  -- Print some stuff after the network is built.
-  r <- ask
-
-  tick <- getTicksEvent
-  new <- holdDyn 2 tick
-  commitLayer $ ffor new $ \moves ->
-    renderAABB r (V4 0 0 100 255) (V2 100 100)
-
-  evQuit <- getQuitEvent
-  performEvent_ $ liftIO (putStrLn "bye!") <$ evQuit
-  shutdownOn =<< delay 0 evQuit
-
-app :: (ReflexSDL2 t m, MonadReader Renderer m) => m ()
-app = do
-  (_, dynLayers) <- runDynamicWriterT guest
-  r <- ask
-  performEvent_ $ ffor (updated dynLayers) $ \layers -> do
-    rendererDrawColor r $= V4 0 0 0 255
-    clear r
-    sequence_ layers
-    present r
+fallingBall :: Pos -> Y.SF () (Pos, Vel)
+fallingBall y0 = (constant (-9.81) >>> integral) >>> ((integral >>^ (+ y0)) &&& identity)
 
 main :: IO ()
-main = do
-  initializeAll
-  let ogl = defaultOpenGL{ glProfile = Core Debug 3 3 }
-      cfg = defaultWindow{ windowOpenGL      = Just ogl
-                         , windowResizable   = True
-                         , windowHighDPI     = False
-                         , windowInitialSize = V2 640 480
-                         }
-  window <- createWindow "reflex-sdl2-exe" cfg
-  void $ glCreateContext window
+main =
+  reactimate (return ())
+                  (\ _ -> threadDelay 100000 >> return (0.1, Nothing))
+                  (\ _  -> render)
+                  (fallingBall 10.0)
 
-  putStrLn "creating renderer..."
-  r <- createRenderer window (-1) defaultRenderer
-  rendererDrawBlendMode r $= BlendAlphaBlend
-  -- Host the network with an example of how to embed your own effects.
-  -- In this case it's a simple reader.
-  MyHost.myHost $ runReaderT app r
-  destroyRenderer r
-  destroyWindow window
-  quit
+render (pos, vel) = putStrLn ("pos: " ++ show pos ++ ", vel: " ++ show vel) >> return False
